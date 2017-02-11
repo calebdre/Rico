ae5d4bfa297a503aa821cf7b3d00ef688254d724
HarshaGoli <harshagoli@gmail.com>
2017-02-11 17:54:26 -0500

    Now compiles all commits on user's repositories

--- expertise.py
+++ expertise.py
@@ -13,4 +13,13 @@ def getuserrepos(username='arshbot'):
 		print list_of_repos
 		return list_of_repos
 
-getuserrepos()
+def getusergitcommits(username='arshbot'):
+	repositories = getuserrepos(username)
+	commitcount=0
+	for r in repositories:
+		rc = requests.get('https://api.github.com/repos/'+username+'/'+r+'/commits')
+		repos=rc.json()
+		commitcount+=len(repos)
+	print commitcount
+
+getusergitcommits()
