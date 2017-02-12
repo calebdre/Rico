e9b8b3c4b59aa369f7692efd3ca1e17bfa32f8da
HarshaGoli <harshagoli@gmail.com>
2017-02-12 11:02:30 -0500

    rico returns a list

--- rico.py
+++ rico.py
@@ -3,10 +3,10 @@ github_score = __import__('getgithubscore')
 so_score = __import__('stackovflodata')
 
 def score(tags={'listing_tags':[], 'candidate_tags':[]}, ghusername='arshbot', name='Harsha Goli', location='Atlanta, GA, United States'):
-	j = jobmatch.score(tags['listing_tags'], tags['candidate_tags'])
-	g = github_score.final_score(ghusername)
-	s = so_score.final_score(name, location)
-	return j+g+s
+	job_match_score = jobmatch.score(tags['listing_tags'], tags['candidate_tags'])
+	git_hub_score = github_score.final_score(ghusername)
+	s_o_score = so_score.final_score(name, location)
+	return [job_match_score, git_hub_score, s_o_score]
 
 #tags = {'listing_tags':['Swift', 'Python', 'Java'], 'candidate_tags':['Swift', 'Python', 'perl', 'shot']}
 #print(score(tags, 'youngvz', 'Viraj Shaw', 'Atlanta, GA, United States'))
