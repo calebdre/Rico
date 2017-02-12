00bb13a5c363372c013f45bb9f336c2282df9de7
HarshaGoli <harshagoli@gmail.com>
2017-02-12 10:39:45 -0500

    took competency out of the loop

--- rico.py
+++ rico.py
@@ -1,9 +1,12 @@
 jobmatch = __import__('jobmatch')
-competency = __import__('competency')
+github_score = __import__('getgithubscore')
+so_score = __import__('stackovflodata')
+
 def score(tags={'listing_tags':[], 'candidate_tags':[]}, ghusername='arshbot', name='Harsha Goli', location='Atlanta, GA, United States'):
 	j = jobmatch.score(tags['listing_tags'], tags['candidate_tags'])
-	c = competency.score(ghusername, name, location)
-	return j+c
+	g = github_score.final_score(ghusername)
+	s = so_score.final_score(name, location)
+	return j+g+s
 
 tags = {'listing_tags':['Swift', 'Python', 'Java'], 'candidate_tags':['Swift', 'Python', 'perl', 'shot']}
 print(score(tags, 'youngvz', 'Viraj Shaw', 'Atlanta, GA, United States'))
