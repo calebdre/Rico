1e953d3c7a294a4e7cfad6807f75858899a96e83
HarshaGoli <harshagoli@gmail.com>
2017-02-12 02:59:36 -0500

    Committing because I have too

--- getgitinfo.py
+++ getgitinfo.py
@@ -4,7 +4,15 @@ from requests.auth import HTTPBasicAuth
 list_of_repos = []
 fork_score = 0
 star_score = 0
-commitcount = 0
+commit_score = 0
+
+def getgitinfo(username='arshbot'):
+    get_user_repos(username)
+    #print list_of_repos
+    get_num_of_github_commits(username)
+    get_num_of_stars_and_forks(username)
+    #print 'fork_score: '+str(fork_score)+', star_score: '+str(star_score)+', comment_score: '+str(commit_score)
+    return {'fork_score': fork_score, 'star_score': star_score, 'comment_score': commit_score}
 
 def get_user_repos(username='arshbot'):
     global list_of_repos
@@ -16,7 +24,7 @@ def get_user_repos(username='arshbot'):
     elif str(r) == '<Response [200]>':
         for r in repos:
             list_of_repos.append(r['name'])
-        print list_of_repos
+        #print list_of_repos
         return list_of_repos
 
 def get_num_of_stars_and_forks(username='arshbot'):
@@ -30,18 +38,11 @@ def get_num_of_stars_and_forks(username='arshbot'):
 
 def get_num_of_github_commits(username='arshbot'):
     global list_of_repos
-    global commitcount
+    global commit_score
     for r in list_of_repos:
         rc = requests.get('https://api.github.com/repos/'+username+'/'+r+'/commits', auth=HTTPBasicAuth('arshbot', 'e172a7f23e13fc855016bb391bd5f504f3f13d86'))
         if str(r) == '<Response [404]>':
             print "Shit ain't found for your repo: "+r
             break
         repos=rc.json()
-        commitcount+=len(repos)
-
-get_user_repos('arshbot')
-print list_of_repos
-get_num_of_github_commits()
-get_num_of_stars_and_forks()
-print 'fork_score: '+str(fork_score)+', star_score: '+str(star_score)+', comment_score: '+str(commitcount)
-git_info = {'fork_score': fork_score, 'star_score': star_score, 'comment_score':comment_score}
+        commit_score+=len(repos)
