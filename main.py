11665b4b1979b3db260ccae92b534481f3cdd55c
Caleb Lewis <caleb_dre@yahoo.com>
2017-02-11 20:28:21 -0500

    added devpost

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from helpers import *
 from nltk.corpus import stopwords
 import textract
 import os
+import re
 import PyPDF2
+import devpost
 
 app = Flask(__name__)
 stop_words = stopwords.words("english")
@@ -14,6 +16,10 @@ cwd = os.getcwd()
 @app.route("/")
 def index():
     pass
+
+@app.route("/devpost")
+def devpost_func():
+    return to_json(devpost.get_tags("caleb_dre"))
 
 @app.route("/parse", methods=["POST"])
 def parse():
@@ -29,16 +35,17 @@ def parse():
         resume += " " + pageObj.extractText()
 
     job_description = job_description.replace(",", "").replace("(", "").replace(")", "")
-    resume = resume.replace(",", "").replace("(", "").replace(")", "")
+    resume = resume.replace(",", "").replace("(", "").replace(")", "").replace("\n", "")
 
     stripped_job_description = [word.lower().strip() for word in job_description.split() if word not in stop_words]
     stripped_resume = [word.lower().strip() for word in resume.split() if word not in stop_words]
 
     matches = []
+
     for i, word in enumerate(stripped_job_description):
         for word2 in stripped_resume:
             if word2 in word:
-                matches.append(word);
+                matches.append(word)
 
     matches = list(set(matches))
     return to_json(matches)
